bf1f485020f4606bf81d9a5bcabc6a9d96fd3a95
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-07-08 16:33:37 -0400

    get conn str

---
+++ S26W9DisconnectedModel/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace S26W9DisconnectedModel
+{
+    public class Data
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+        }
+    }
+}
