af06a883ad63623f1940b3a2679255d9f66fb59f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-07-08 16:48:09 -0400

    get all prods

--- S26W9DisconnectedModel/Data.cs
+++ S26W9DisconnectedModel/Data.cs
@@ -13,5 +13,19 @@ namespace S26W9DisconnectedModel
         {
             return ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
         }
+
+        public DataTable GetAllProducts()
+        {
+            SqlConnection conn = new SqlConnection(GetConnectionString());
+
+            string query = "select ProductID, ProductName, UnitPrice, UnitsInStock from Products";
+            SqlDataAdapter adp = new SqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+
+            adp.Fill(ds, "Products");
+
+            return ds.Tables["Products"]!;
+        }
     }
 }
