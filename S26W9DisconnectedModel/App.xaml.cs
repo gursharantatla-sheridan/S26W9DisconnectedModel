c9671aa3c0b3b50a9ecfe6dc1412ab4a540e6d9f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-07-08 16:19:48 -0400

    Add project files.

---
+++ S26W9DisconnectedModel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace S26W9DisconnectedModel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
